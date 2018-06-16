886dfb2a2be8eaba0b7fa5edf42383ecb69da9f6
uncurry <zarcho65@gmail.com>
2018-06-17 00:36:25 +0300

    Created views and partials.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord7
   belongs_to :category
-  validates :title, :content, :category_id, prescence :true 
+  validates :title, :content, :category_id, prescence :true
+  has_many :line_items, inverse_of: :order
 end
